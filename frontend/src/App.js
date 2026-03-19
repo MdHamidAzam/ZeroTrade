c174853fbb84163d208f767f8a0404e2124ac863
Md Hamid Azam <hm2563749@gmail.com>
2026-03-19 12:55:39 +0530

    updating frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./landing_page/signup/Signup";
 import Login from "./landing_page/signup/Login";
-import HomePage from "./landing_page/Home/HomePage";
-import AboutPage from "./landing_page/about/AboutPage";
-import ProductPage from "./landing_page/products/ProductPage";
-import PricingPage from "./landing_page/pricing/PricingPage";
-import SupportPage from "./landing_page/supports/SupportPage";
-import NotFound from "./landing_page/NotFound";
+import Home from "./dashboard/components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/products" element={<ProductPage />} />
-        <Route path="/pricing" element={<PricingPage />} />
-        <Route path="/supports" element={<SupportPage />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
