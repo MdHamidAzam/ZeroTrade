c1e7849280c7e0b1e5872829e0a0a91014c0bfcb
Md Hamid Azam <hm2563749@gmail.com>
2026-03-19 14:53:34 +0530

    updated navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-
   const token = localStorage.getItem("token");
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -10,13 +9,17 @@ function Navbar() {
   };
 
   return (
-    <nav className="navbar navbar-expand-lg border-bottom"
-      style={{ backgroundColor: "#fff" }}>
-      
+    <nav
+      className="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#fff" }}
+    >
       <div className="container">
-
         <Link className="navbar-brand" to="/">
-          <img src="/media/images/zerotrade.png" style={{ width: "50%" }} alt="Logo" />
+          <img
+            src={process.env.PUBLIC_URL + "/media/images/zerotrade.png"}
+            style={{ width: "50%" }}
+            alt="Logo"
+          />
         </Link>
 
         <button
@@ -29,33 +32,43 @@ function Navbar() {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-
           <ul className="navbar-nav mb-lg-0">
-
             <li className="nav-item">
-              <Link className="nav-link active" to="about">About</Link>
+              <Link className="nav-link active" to="about">
+                About
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link active" to="products">Products</Link>
+              <Link className="nav-link active" to="products">
+                Products
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link active" to="pricing">Pricing</Link>
+              <Link className="nav-link active" to="pricing">
+                Pricing
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link active" to="supports">Supports</Link>
+              <Link className="nav-link active" to="supports">
+                Supports
+              </Link>
             </li>
 
             {!token ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/login">Login</Link>
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
                 </li>
 
                 <li className="nav-item">
-                  <Link className="nav-link" to="/signup">Signup</Link>
+                  <Link className="nav-link" to="/signup">
+                    Signup
+                  </Link>
                 </li>
               </>
             ) : (
@@ -68,9 +81,7 @@ function Navbar() {
                 </button>
               </li>
             )}
-
           </ul>
-
         </div>
       </div>
     </nav>
